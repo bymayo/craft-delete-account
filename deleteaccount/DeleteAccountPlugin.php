548b6b84d653f3ebd0bf0647999d57d486d16cdd
Jason Mayo <jason@bymayo.co.uk>
2018-05-25 11:21:09 +0100

    Fixed offset error in settings

--- deleteaccount/DeleteAccountPlugin.php
+++ deleteaccount/DeleteAccountPlugin.php
@@ -38,12 +38,12 @@ class DeleteAccountPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.1';
+        return '1.0.2';
     }
 
     public function getSchemaVersion()
     {
-        return '1.0.1';
+        return '1.0.2';
     }
 
     public function getDeveloper()
